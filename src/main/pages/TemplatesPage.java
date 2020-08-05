0cc436fcd70df786d91755ea0adf285510f173a0
kevalporiya <52760573+kevalporiya@users.noreply.github.com>
2020-08-05 15:28:06 -0400

    Update TemplatesPage.java
    changes has been made

--- src/main/pages/TemplatesPage.java
+++ src/main/pages/TemplatesPage.java
@@ -5,11 +5,18 @@ public class IndividualTemplatesPage {
 	// Add XPaths here
 	
 	
+	WebElement signUp = driver.findElement(By.xpath("//div[@id='featured']//div[@class='staticStyles__templatesContainer--2dQJ3']//div[2]//div[1]//div[1]//div[1]//div[2]//div[1]//center[1]//div[1]//a[1]//button[1]"))
 	public IndividualTemplatesPage(){
 		this.selenium = Selenium();
 	}
 	
 	// Create a function that clicks on the Sign Up button
 	
-	
+	public void clickOnSignUpButton()
+	{
+		Actions builder = new Actions(driver);
+		Action mouseOverSignUpButton = builder.moveToElement(signUp).click().build().perform();
+                       
+                      
+	}
 }
