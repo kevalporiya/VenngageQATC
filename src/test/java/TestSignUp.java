84f6a78b432d69a7fe848f3fe55b75f220fea179
kevalporiya <52760573+kevalporiya@users.noreply.github.com>
2020-08-05 14:37:56 -0400

    Update TestSignUp.java

--- src/test/java/TestSignUp.java
+++ src/test/java/TestSignUp.java
@@ -2,16 +2,32 @@ public class TestSignUp {
 	
 	private Account account;
 	private SignUpPage signUpPage;
+	public static WebDriver driver;
 	
 	@Before
 	public void setUp(){
 		// Instantiate page objects here
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium_MyPractise\\TestFirmexKeval\\chromedriver.exe");
+			driver = new ChromeDriver();
+		driver.get("https://venngage.com/templates/layouts");
 		
 	}
 	
 	@Test
 	public void testSignUp(){
 		// Write test steps here
-		qe2e1
+		
+		Actions a = new Actions(driver);
+		a.moveto
+		
+		driver.findElement(By.id("user_first_name")).sendKeys("QA");
+		driver.findElement(By.id("user_last_name")).sendKeys("Test");
+		driver.findElement(By.id("user_email")).sendKeys("qa.123@venngage.com");
+		driver.findElement(By.id("user_password")).sendKeys("123456");
+		driver.findElement(By.id("btn_register")).click();
+		
+		
+		
 	}
 }
+
