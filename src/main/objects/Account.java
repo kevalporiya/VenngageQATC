c87a3c90c1ec336b1aec838c8bd38c867e34f6f4
kevalporiya <52760573+kevalporiya@users.noreply.github.com>
2020-08-05 14:49:56 -0400

    Update Account.java
    Changes made to Account.java class file

--- src/main/objects/Account.java
+++ src/main/objects/Account.java
@@ -8,7 +8,50 @@ public class Account {
 	* Password
 	**/
 	
+	private String FirstName;
+	private String LastName;
+	private String Email;
+	private String Password;
 	
-	
+	public Account(String FirstName , String LastName , String Email , String Password)
+	{
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.Email = Email;
+		this.Password= Password;	
+	}	
 	// Create the necessary Getter functions
+	
+	public String  getFirstName()
+	{
+        return this.FirstName;
+    	}
+   	 public void setFirstName(String firstname) {
+       	 this.FirstName = firstname;
+   	 }
+	
+	public String  getLastName()
+	{
+        return this.LastName;
+    	}
+   	 public void setLastName(String lastname) {
+       	 this.LastName = lastname;
+   	 }
+	
+	public String  getEmail()
+	{
+        return this.Email;
+    	}
+   	 public void setEmail(String email) {
+       	 this.Email = email;
+   	 }
+	
+	public String  getPassword()
+	{
+        return this.Password;
+    	}
+   	 public void setPassword(String password) {
+       	 this.Password = password;
+   	 }
+	
 }
